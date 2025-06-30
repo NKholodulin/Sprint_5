072a3cc598e441da007df50818831fe2df6b2203
holod <holodulinn@mail.ru>
2025-07-01 00:43:39 +0400

    Добавлен тестовый класс OrderTest

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class OrderTest {
+    private WebDriver driver;
+    MainPage mainPage;
+    OrderPage orderPage;
+
+    @BeforeEach
+    void setUp() {
+        // создали драйвер для браузера Chrome или Firefox
+        driver = new ChromeDriver();
+        //driver = new FirefoxDriver();
+        // перешли на страницу тестового приложения
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+        // создали объект класса страницы с вопросами
+        mainPage = new MainPage(driver);
+        orderPage = new OrderPage(driver);
+    }
+
+    @Test
+    void orderHeader() {
+        mainPage.cookieConfirm();
+        mainPage.clickOrderButtonHeader();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.textToBe(orderPage.getOrderHeader(), "Для кого самокат"));
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        // Закрой браузер
+        driver.quit();
+    }
 }

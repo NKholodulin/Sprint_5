5b719c09f80f10ac2d5325289ca9e009e7d4b360
holod <holodulinn@mail.ru>
2025-07-01 01:28:27 +0400

    Добавлен ввод имени в класс OrderTest

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,42 +1,70 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.stream.Stream;
 
 public class OrderTest {
     private WebDriver driver;
     MainPage mainPage;
     OrderPage orderPage;
 
-    @BeforeEach
-    void setUp() {
+    @ParameterizedTest
+    @MethodSource("locatorProvider")
+    void orderHeader(String browser, String name) {
+
         // создали драйвер для браузера Chrome или Firefox
-        driver = new ChromeDriver();
-        //driver = new FirefoxDriver();
+        if ("chrome".equalsIgnoreCase(browser)) {
+            driver = new ChromeDriver();
+        } else if ("firefox".equalsIgnoreCase(browser)) {
+            driver = new FirefoxDriver();
+        } else {
+            throw new IllegalArgumentException("Неизвестный браузер: " + browser);
+        }
         // перешли на страницу тестового приложения
         driver.get("https://qa-scooter.praktikum-services.ru/");
         // создали объект класса страницы с вопросами
         mainPage = new MainPage(driver);
         orderPage = new OrderPage(driver);
-    }
 
-    @Test
-    void orderHeader() {
+        //подтвердили куки
         mainPage.cookieConfirm();
+
+        //нажали Заказать в хедере страницы
         mainPage.clickOrderButtonHeader();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
         wait.until(ExpectedConditions.textToBe(orderPage.getOrderHeader(), "Для кого самокат"));
-
+        //заполнили имя
+        orderPage.inputName(name);
     }
 
     @AfterEach
     public void tearDown() {
         // Закрой браузер
         driver.quit();
+    }
+
+    static Stream<Arguments> locatorProvider() {
+        return Stream.of(
+                // Реализуй тестовые данные
+                Arguments.of(
+                        "chrome",
+                        "Никита"
+                ),
+                Arguments.of(
+                        "firefox",
+                        "Виктория"
+                )
+        );
     }
 }

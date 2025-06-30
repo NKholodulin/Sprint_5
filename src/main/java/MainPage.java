c0a8d4c7e88275ccf5815e6deb6870ef5b16391d
holod <holodulinn@mail.ru>
2025-07-01 00:17:54 +0400

    Исправлено подтверждение куки, добавлен локатор кнопки заказать в хедере, добавлено нажатие кнопки заказать в хедере

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -4,21 +4,21 @@ import org.openqa.selenium.WebDriver;
 public class MainPage {
     private WebDriver driver;
 
-
     private By cookieConfirmLocator = By.id("rcc-confirm-button");
-
-    public By getCookieConfirmLocator() {
-        return cookieConfirmLocator;
-    }
-
+    private By orderButtonHeader = By.className("Button_Button__ra12g");
 
     public MainPage(WebDriver driver) {
         this.driver = driver;
     }
 
     //метод для нажатия на кнопку принятия куки
-    public void cookieConfirm(By cookieConfirmLocator) {
+    public void cookieConfirm() {
         driver.findElement(cookieConfirmLocator).click();
+    }
+
+    //метод для нажатия на кнопку Заказать в хедере
+    public void clickOrderButtonHeader() {
+        driver.findElement(orderButtonHeader).click();
     }
 
 }

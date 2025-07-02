dc8b038a631fdd7a449e547419d1a745827c3e16
holod <holodulinn@mail.ru>
2025-07-03 00:09:11 +0400

    Добавлен тест на неправильный статус заказа

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -14,6 +14,12 @@ public class MainPage {
     private By logoScooter = By.className("Header_LogoScooter__3lsAR");
     //Логотип яндекса
     private By logoYandex = By.className("Header_LogoYandex__3TSOI");
+    //Кнопка статус заказа
+    private By statusButton = By.className("Header_Link__1TAG7");
+    //Поле ввода номера заказа
+    private By statusInput = By.xpath("//input[@placeholder='Введите номер заказа']");
+    //Кнопка Go
+    private By statusGo = By.xpath("//button[text()='Go!']");
 
     public MainPage(WebDriver driver) {
         this.driver = driver;
@@ -47,6 +53,24 @@ public class MainPage {
     public void clickLogoYandex(){
         driver.findElement(logoYandex).isDisplayed();
         driver.findElement(logoYandex).click();
+    }
+
+    //Метод для получения локатора ввода номера заказа
+    public By getStatusInput() {
+        return statusInput;
+    }
+
+    //Метод для нажатия на кнопку статус
+    public void clickStatus(){
+        driver.findElement(statusButton).isDisplayed();
+        driver.findElement(statusButton).click();
+    }
+    //Метод для заполнения номера заказа
+    public void inputStatus(String orderNumber){
+        driver.findElement(statusInput).isDisplayed();
+        driver.findElement(statusInput).sendKeys(orderNumber);
+        driver.findElement(statusGo).isDisplayed();
+        driver.findElement(statusGo).click();
     }
 
 }
---
+++ src/test/java/OrderNumberNegativeTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class OrderNumberNegativeTest {
+    private WebDriver driver;
+    MainPage mainPage;
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+        // создали объекты классов страниц
+        mainPage = new MainPage(driver);
+    }
+
+    @Test
+    void testFormSubmissionWithActions() {
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+        //подтвердили куки
+        mainPage.cookieConfirm();
+        mainPage.clickStatus();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.elementToBeClickable(mainPage.getStatusInput()));
+
+        mainPage.inputStatus("12345");
+        //Дождались открытия страницы заказа
+        wait.until(ExpectedConditions.urlToBe("https://qa-scooter.praktikum-services.ru/track?t=12345"));
+        
+        assertTrue(driver.findElements(By.xpath(".//img[@alt='Not found']")).size() > 0, "Элемент с alt='Not found' отсутствует на странице");
+
+
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+}
